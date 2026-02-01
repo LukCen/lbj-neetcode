b718d3f13f1325b08560ae10642046c4a9bcba98
lbjcena <lbjcena@gmail.com>
2026-02-01 17:46:10 +0100

    valid-parentheses added

--- two-sum/script.js
+++ two-sum/script.js
@@ -1,7 +1,5 @@
 // Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
-
 // You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
-
 // Return the answer with the smaller index first.
 
 const numbers_1 = [3, 4, 5, 6]
@@ -16,23 +14,25 @@ function twoSum(nums, target) {
   }
 }
 
-console.log(twoSum(numbers_1, target_1))
-
-
 /**
- * 
  * @param {number[]} nums 
  * @param {number[]} target 
  */
+
+const numbers_2 = [0, 2, 10, 4, 1, 7]
+const target_2 = 14
 
 function twoSum_2(nums, target) {
   const storage = new Map()
   let indexOfComponent = null
   for (let i = 0; i < nums.length; i++) {
     const complement = target - nums[i]
-    if (!storage.has(complement)) storage.set(nums[i], i)
+    if (!storage.has(complement)) {
+      storage.set(nums[i], i)
+      console.log(storage)
+    }
     else return [i, storage.get(complement)]
   }
   return [indexOfComponent, storage[1]]
 }
-console.log(twoSum_2(numbers_1, target_1))
+console.log(twoSum_2(numbers_2, target_2))
---
+++ valid-parentheses/script.js
@@ -0,0 +1,53 @@
+// You are given a string s consisting of the following characters: '(', ')', '{', '}', '[' and ']'.
+
+// The input string s is valid if and only if:
+
+// Every open bracket is closed by the same type of close bracket.
+// Open brackets are closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+// Return true if s is a valid string, and false otherwise.
+
+const string_1 = "[[[]"
+
+/**
+ * 
+ * @param {string} string 
+ */
+
+function isValid(string) {
+  const stack = []
+  const intoStrings = string.split("")
+  const openingBrackets = ["(", "[", "{"]
+  const closingBrackets = [")", "]", "}"]
+
+  // if the entry string is empty/invalid or contains an odd amount of characters, matching is impossible, as there will be at least one bracket left without a pair - early return
+  if (!string || string.length % 2 === 1) return false
+
+  // if the string contains only opening or closing brackets, matching is impossible - early return
+  if (intoStrings.every((i) => openingBrackets.includes(i)) || intoStrings.every((i) => closingBrackets.includes(i))) return false
+
+  for (let i = 0; i <= intoStrings.length - 1; i++) {
+    const currentCharacter = intoStrings[i]
+    // if current character is an opening bracket, add it to stack
+    if (openingBrackets.includes(currentCharacter)) {
+      stack.push(currentCharacter)
+      // if it's a closing bracket, compare it to the LAST element in stack, see if a match
+    } else if (closingBrackets.includes(currentCharacter)) {
+      // look up the indices of currently handled elements in their respective arrays - for a valid pair, they must be equal
+      const isMatch = closingBrackets.indexOf(currentCharacter) === openingBrackets.indexOf(stack[stack.length - 1])
+      if (isMatch) {
+        // remove the last element from the stack if it matches
+        stack.pop()
+      } else {
+        // even one mismatch is a failure according to the task requirements - early return to cover for that
+        return false
+      }
+    }
+  }
+  // probably a bit unnecessary here, but it works so I'll leave it
+  return true ? stack.length === 0 : false
+}
+
+console.log(isValid(string_1))
+
+
